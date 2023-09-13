d289d4993cf1fdedceabd96e7ee6377a8b67776b
AlexandrLeonovdnd <squirtle007@mail.ru>
2023-09-13 10:54:21 +0300

    AT-lesson6 Selenium

---
+++ src/main/java/AddElementsTest.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AddElementsTest extends BaseTest {
+    @Test
+    public void verifyAddElements() {
+        driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+        driver.findElement(By.xpath("//button[@onclick='addElement()']")).click();
+        driver.findElement(By.xpath("//button[@onclick='addElement()']")).click();
+        driver.findElement(By.xpath("//button[@class='added-manually']")).click();
+        int countElements = driver.findElements(By.xpath("//button[@onclick='addElement()']")).size();
+        Assert.assertEquals(countElements, 1);
+    }
+}
